5ff963e493a38eb263bd6130d1925bda5f443219
yuzuki-aritomo <yuzuki.aritomo@gmail.com>
2020-08-19 02:23:44 +0900

    commit

---
+++ Sort_2/Sort_2/QSort2.cs
@@ -0,0 +1,82 @@
+﻿// 1191100011 有友柚樹
+using System;
+
+class QuickSort
+{
+    public static int check;
+    public static void Sort(int[] data, int start, int end)
+    {
+        if (end - start <= 1 ) // 要素数が1以下なら終了
+        {
+            return;
+        }
+        //Console.WriteLine("----------");
+        int p = data[start]; // 先頭要素をピボット p とする
+        int i = start, j = end;
+        while (true)
+        {
+            int tmp;
+            if(p > data[i])
+            {
+                i++;
+            }
+            if(p <= data[j])
+            {
+                j--;
+            }
+            if(p <= data[i] && p > data[j])
+            {
+                
+                tmp = data[i];
+                data[i] = data[j];
+                data[j] = tmp;
+                for (int l = 0; l < data.Length; l++)
+                {
+                    Console.Write("{0} ", data[l]);
+                }
+                Console.WriteLine();
+            }
+            
+            if (i >= j)
+            {
+                Console.WriteLine("----------");
+                check = i;
+                //Console.WriteLine(check);
+                break;
+            }
+        }
+        //Console.WriteLine(check);
+
+        //Console.WriteLine("----------");
+        Sort(data, start, check); // workのピボットの前後に対し、
+        Sort(data, check + 1, end); // それぞれSortを再帰呼び出し
+    }
+}
+
+class SortMain
+{
+    const int N_DATA = 20;
+    public static void Main()
+    {
+        int[] data = new int[N_DATA];
+        Random rand = new Random();
+        Console.WriteLine("before sort");
+        for (int i = 0; i < data.Length; i++)
+        {
+            data[i] = rand.Next(N_DATA);
+            Console.Write("{0} ", data[i]);
+        }
+        Console.WriteLine();
+        QuickSort.Sort(data, 0, data.Length-1);
+        Console.WriteLine("after sort");
+        for (int i = 0; i < data.Length; i++)
+        {
+            Console.Write("{0} ", data[i]);
+        }
+        Console.WriteLine();
+    }
+}
+
+//今回の課題を完成させることが出来ませんでした。
+//なぜ思い通りに動かないのかデバックをしたのですがどれもうまくいきませんでした。
+//
---
+++ 総合課題/総合課題/CodeFile1.cs
@@ -0,0 +1,106 @@
+﻿//1191100011
+using System;
+
+class Code
+{
+    //最小公倍数を求める
+    public static int Lcm(int a, int b)
+    {
+        return a * b / Gcd(a, b);
+    }
+    // ユークリッドの互除法 
+    public static int Gcd(int a, int b)
+    {
+        if (a < b)
+            // 引数を入替えて自分を呼び出す
+            return Gcd(b, a);
+        while (b != 0)
+        {
+            var remainder = a % b;
+            a = b;
+            b = remainder;
+        }
+        return a;
+    }
+    //落ちるポケットを求める
+    public static string Hole(int w_sum, int h_sum)
+    {
+        string pocket;
+        if (h_sum % 2 == 0)
+        {
+            if (w_sum % 2 == 0)
+            {
+                pocket = "A";
+                return pocket;
+            }
+            else
+            {
+                pocket = "B";
+                return pocket;
+            }
+        }
+        else
+        {
+            if (w_sum % 2 == 0)
+            {
+                pocket = "C";
+                return pocket;
+            }
+            else
+            {
+                pocket = "D";
+                return pocket;
+            }
+        }
+    }
+    //----------------------------課題１------------------------------
+    public void Code_1()
+    {
+        //前提条件
+        int w = 6;
+        int h = 3;
+        //int theta = 45;
+
+        //h,wの最小公倍数を求める
+        int x = Lcm(w, h);
+        
+        //あたった回数を求める
+        int w_sum = x / w - 1;
+        int h_sum = x / h - 1;
+        int sum = w_sum + h_sum;
+        string pocket = Hole(w_sum+1,h_sum+1);
+
+        Console.WriteLine("クッションにあたった回数：{0}",sum);
+        Console.WriteLine("入ったポケット：{0}",pocket);
+    }
+    //----------------------------課題２------------------------------
+    public void Code_2()
+    {
+        //theta = 45
+        int w = 5;
+        int h = 3;
+
+        //h,wの最小公倍数を求める
+        int x = Lcm(w, h);
+
+        //あたった回数を求める
+        int w_sum = x / w - 1;
+        int h_sum = x / h - 1;
+        int sum = w_sum + h_sum;
+        string pocket = Hole(w_sum + 1, h_sum + 1);
+
+        Console.WriteLine("クッションにあたった回数：{0}", sum);
+        Console.WriteLine("入ったポケット：{0}", pocket);
+    }
+}
+class CodeMain
+{
+    public static void Main()
+    {
+        Code A = new Code();
+        Console.WriteLine("課題１");
+        A.Code_1();
+        Console.WriteLine("課題2");
+        A.Code_2();
+    }
+}
